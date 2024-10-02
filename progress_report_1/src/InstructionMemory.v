1710aed2c0cc2ae945266d3f8a41a1a2fc0b4a02
Matthew Quijano <mq022002@gmail.com>
2024-10-02 15:25:17 -0400

    refactor: Migrate test bench to a separate file for instruction memory
    #1

--- progress_report_1/src/InstructionMemory.v
+++ progress_report_1/src/InstructionMemory.v
@@ -17,26 +17,3 @@ module InstructionMemory (
 
     assign instruction = memory[address[9:0]];
 endmodule
-
-// Author(s): Matthew Quijano
-module instruction_memory_tb;
-    reg [15:0] address;
-    wire [15:0] instruction;
-
-    InstructionMemory uut (
-        .address(address),
-        .instruction(instruction)
-    );
-
-    initial begin
-        address = 16'h0000;
-        #10 address = 16'h0002;
-        #10 address = 16'h0004;
-        #10 address = 16'h0006;
-        #10 $finish;
-    end
-
-    initial begin
-        $monitor("Time: %0d | Address: %h | Instruction: %h", $time, address, instruction);
-    end
-endmodule
