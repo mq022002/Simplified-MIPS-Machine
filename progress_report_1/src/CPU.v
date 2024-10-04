848ca201358fd57bdc3b67880433d0763677a77e
Matthew Quijano <mq022002@gmail.com>
2024-10-04 09:20:51 -0400

    refactor: Seperate test bench for CPU and adjust Makefile accordingly
    #1

--- progress_report_1/src/CPU.v
+++ progress_report_1/src/CPU.v
@@ -80,28 +80,3 @@ module ControlUnit (
         endcase
     end
 endmodule
-
-// Author(s): Joey Conroy, Abbie Mathew
-module CPU_tb;
-    reg clock;
-    reg reset;
-
-    CPU uut (
-        .clock(clock),
-        .reset(reset)
-    );
-
-    initial begin
-        clock = 0;
-        reset = 1;
-        #10 reset = 0;
-        #100 $finish;
-    end
-
-    always #5 clock = ~clock;
-
-    initial begin
-        $monitor("Time: %0d | PC: %h | Instruction: %h | ALUOut: %h | Zero: %b", 
-                 $time, uut.PC, uut.instruction, uut.ALUOut, uut.Zero);
-    end
-endmodule
