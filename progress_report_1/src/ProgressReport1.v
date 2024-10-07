23faa7633d06d1015bc58d43de34683d25d2e3f6
Matthew Quijano <mq022002@gmail.com>
2024-10-07 11:22:44 -0400

    feat: Simplify all modules, expand ControlUnit, confirmed working output
    - The output is now correct, leaving only transitioning ALU to a gate-level implementation. Something we already had, but I think this will be an easier refactor.
    - Output:
    Clock PC   IR                 WD
    1      0   0111000100001111   15 (0000000000001111)
    0      2   0111001000000111    7 (0000000000000111)
    1      2   0111001000000111    7 (0000000000000111)
    0      4   0010011011000000    7 (0000000000000111)
    1      4   0010011011000000    7 (0000000000000111)
    0      6   0001011110000000    8 (0000000000001000)
    1      6   0001011110000000    8 (0000000000001000)
    0      8   0011101110000000   15 (0000000000001111)
    1      8   0011101110000000   15 (0000000000001111)
    0     10   0000101111000000   22 (0000000000010110)
    1     10   0000101111000000   22 (0000000000010110)
    0     12   0100101101000000  65504 (1111111111100000)
    1     12   0100101101000000  65504 (1111111111100000)
    0     14   0110111001000000    0 (0000000000000000)
    1     14   0110111001000000    0 (0000000000000000)
    0     16   0110101101000000    1 (0000000000000001)
    1     16   0110101101000000    1 (0000000000000001)
    0     18   1111111111111111   22 (0000000000010110)
    1     18   1111111111111111   22 (0000000000010110)
    0     20   xxxxxxxxxxxxxxxx    x (xxxxxxxxxxxxxxxx)
    1     20   xxxxxxxxxxxxxxxx    x (xxxxxxxxxxxxxxxx)
    0     22   xxxxxxxxxxxxxxxx    x (xxxxxxxxxxxxxxxx)
    1     22   xxxxxxxxxxxxxxxx    x (xxxxxxxxxxxxxxxx)
    0     24   xxxxxxxxxxxxxxxx    x (xxxxxxxxxxxxxxxx)
    1     24   xxxxxxxxxxxxxxxx    x (xxxxxxxxxxxxxxxx)
    0     26   xxxxxxxxxxxxxxxx    x (xxxxxxxxxxxxxxxx)
    1     26   xxxxxxxxxxxxxxxx    x (xxxxxxxxxxxxxxxx)
    0     28   xxxxxxxxxxxxxxxx    x (xxxxxxxxxxxxxxxx)
    1     28   xxxxxxxxxxxxxxxx    x (xxxxxxxxxxxxxxxx)
    0     30   xxxxxxxxxxxxxxxx    x (xxxxxxxxxxxxxxxx)
    1     30   xxxxxxxxxxxxxxxx    x (xxxxxxxxxxxxxxxx)
    0     32   xxxxxxxxxxxxxxxx    x (xxxxxxxxxxxxxxxx)
    1     32   xxxxxxxxxxxxxxxx    x (xxxxxxxxxxxxxxxx)
    0     34   xxxxxxxxxxxxxxxx    x (xxxxxxxxxxxxxxxx)
    
    #1

--- progress_report_1/src/ProgressReport1.v
+++ progress_report_1/src/ProgressReport1.v
@@ -1,261 +1,208 @@
 // Author(s): Abbie Mathew
 module ALU (
-    input [15:0] A,
-    input [15:0] B,
     input [3:0] ALUControl,
-    output [15:0] ALUOut,
+    input [15:0] A, B,
+    output reg [15:0] ALUOut,
     output Zero
 );
-    wire [15:0] B_inverted;
-    wire carry_in0;
-    wire [15:0] sum;
-    wire [15:0] carry_out;
-
-    assign B_inverted = (ALUControl == 4'b0110) ? ~B : B;
-    assign carry_in0 = (ALUControl == 4'b0110) ? 1'b1 : 1'b0;
-
-    ALU1 alu0 (A[0], B_inverted[0], carry_in0, sum[0], carry_out[0]);
-    ALU1 alu1 (A[1], B_inverted[1], carry_out[0], sum[1], carry_out[1]);
-    ALU1 alu2 (A[2], B_inverted[2], carry_out[1], sum[2], carry_out[2]);
-    ALU1 alu3 (A[3], B_inverted[3], carry_out[2], sum[3], carry_out[3]);
-    ALU1 alu4 (A[4], B_inverted[4], carry_out[3], sum[4], carry_out[4]);
-    ALU1 alu5 (A[5], B_inverted[5], carry_out[4], sum[5], carry_out[5]);
-    ALU1 alu6 (A[6], B_inverted[6], carry_out[5], sum[6], carry_out[6]);
-    ALU1 alu7 (A[7], B_inverted[7], carry_out[6], sum[7], carry_out[7]);
-    ALU1 alu8 (A[8], B_inverted[8], carry_out[7], sum[8], carry_out[8]);
-    ALU1 alu9 (A[9], B_inverted[9], carry_out[8], sum[9], carry_out[9]);
-    ALU1 alu10 (A[10], B_inverted[10], carry_out[9], sum[10], carry_out[10]);
-    ALU1 alu11 (A[11], B_inverted[11], carry_out[10], sum[11], carry_out[11]);
-    ALU1 alu12 (A[12], B_inverted[12], carry_out[11], sum[12], carry_out[12]);
-    ALU1 alu13 (A[13], B_inverted[13], carry_out[12], sum[13], carry_out[13]);
-    ALU1 alu14 (A[14], B_inverted[14], carry_out[13], sum[14], carry_out[14]);
-    ALU1 alu15 (A[15], B_inverted[15], carry_out[14], sum[15], carry_out[15]);
-
-    wire [15:0] and_result, or_result, nor_result, nand_result;
-    genvar i;
-    generate
-        for (i = 0; i < 16; i = i + 1) begin
-            and (and_result[i], A[i], B[i]);
-            or  (or_result[i], A[i], B[i]);
-            nor (nor_result[i], A[i], B[i]);
-            nand (nand_result[i], A[i], B[i]);
-        end
-    endgenerate
-
-    assign ALUOut = (ALUControl == 4'b0000) ? and_result :
-                    (ALUControl == 4'b0001) ? or_result :
-                    (ALUControl == 4'b0010 || ALUControl == 4'b0110) ? sum :
-                    (ALUControl == 4'b0111) ? (A < B ? 16'h0001 : 16'h0000) :
-                    (ALUControl == 4'b1100) ? nor_result :
-                    (ALUControl == 4'b1101) ? nand_result :
-                    16'h0000;
-
-    assign Zero = (ALUOut == 16'b0);
-endmodule
-
-// Author(s): Abbie Mathew
-module ALU1 (
-    input A,
-    input B,
-    input carry_in,
-    output Result,
-    output carry_out
-);
-    wire sum, carry1, carry2, carry3;
-
-    xor (sum, A, B, carry_in);
-    and (carry1, A, B);
-    and (carry2, A, carry_in);
-    and (carry3, B, carry_in);
-    or  (carry_out, carry1, carry2, carry3);
-
-    assign Result = sum;
+    always @(*)
+        case (ALUControl)
+            4'b0000: ALUOut = A & B;
+            4'b0001: ALUOut = A | B;
+            4'b0010: ALUOut = A + B;
+            4'b0110: ALUOut = A - B;
+            4'b0111: ALUOut = (A < B) ? 1 : 0;
+            4'b1100: ALUOut = ~(A | B);
+            4'b1101: ALUOut = ~(A & B);
+            default: ALUOut = 16'h0000;
+        endcase
+    assign Zero = (ALUOut == 0);
 endmodule
 
 // Author(s): Abbie Mathew
 module RegisterFile (
-    input [1:0] RR1,
-    input [1:0] RR2,
-    input [1:0] WR,
+    input [1:0] RR1, RR2, WR,
     input [15:0] WD,
     input RegWrite,
     input clock,
-    output [15:0] RD1,
-    output [15:0] RD2
+    output [15:0] RD1, RD2
 );
-    reg [15:0] Regs [3:0];
-
-    initial begin
-        Regs[0] = 16'h0000;
-        Regs[1] = 16'h0000;
-        Regs[2] = 16'h0000;
-        Regs[3] = 16'h0000;
-    end
-
+    reg [15:0] Regs[0:3];
     assign RD1 = Regs[RR1];
     assign RD2 = Regs[RR2];
-
-    always @(negedge clock) begin
-        if (RegWrite && WR != 2'b00) begin
-            Regs[WR] <= WD;
-        end
+    initial begin
+        Regs[0] = 0;
+        Regs[1] = 0;
+        Regs[2] = 0;
+        Regs[3] = 0;
     end
+    always @(negedge clock)
+        if (RegWrite == 1 && WR != 0)
+            Regs[WR] <= WD;
 endmodule
 
 
 // Author(s): Matthew Quijano
 module ControlUnit (
-    input [3:0] opcode,
+    input [3:0] Op,
+    output reg RegDst,
+    output reg ALUSrc,
     output reg RegWrite,
-    output reg [3:0] ALUControl,
-    output reg ALUSrc
+    output reg [3:0] ALUControl
 );
-    always @(*) begin
-        case (opcode)
+    always @(*)
+        case (Op)
             4'b0000: begin
+                RegDst   = 1;
+                ALUSrc   = 0;
                 RegWrite = 1;
-                ALUControl = 4'b0010;
-                ALUSrc = 0;
+                ALUControl   = 4'b0010;
             end
             4'b0001: begin
+                RegDst   = 1;
+                ALUSrc   = 0;
                 RegWrite = 1;
-                ALUControl = 4'b0010;
-                ALUSrc = 1;
+                ALUControl   = 4'b0110;
+            end
+            4'b0010: begin
+                RegDst   = 1;
+                ALUSrc   = 0;
+                RegWrite = 1;
+                ALUControl   = 4'b0000;
+            end
+            4'b0011: begin
+                RegDst   = 1;
+                ALUSrc   = 0;
+                RegWrite = 1;
+                ALUControl   = 4'b0001;
+            end
+            4'b0100: begin
+                RegDst   = 1;
+                ALUSrc   = 0;
+                RegWrite = 1;
+                ALUControl   = 4'b1100;
+            end
+            4'b0101: begin
+                RegDst   = 1;
+                ALUSrc   = 0;
+                RegWrite = 1;
+                ALUControl   = 4'b1101;
+            end
+            4'b0110: begin
+                RegDst   = 1;
+                ALUSrc   = 0;
+                RegWrite = 1;
+                ALUControl   = 4'b0111;
+            end
+            4'b0111: begin
+                RegDst   = 0;
+                ALUSrc   = 1;
+                RegWrite = 1;
+                ALUControl   = 4'b0010;
             end
             default: begin
+                RegDst   = 0;
+                ALUSrc   = 0;
                 RegWrite = 0;
-                ALUControl = 4'b0000;
-                ALUSrc = 0;
+                ALUControl   = 4'b0000;
             end
         endcase
-    end
-endmodule
-
-// Author: Matthew Quijano
-module PC (
-    input clock,
-    input reset,
-    input [15:0] nextPC,
-    output reg [15:0] PC
-);
-    always @(posedge clock or posedge reset) begin
-        if (reset) 
-            PC <= 16'h0000;
-        else 
-            PC <= nextPC;
-    end
-endmodule
-
-// Author(s): Matthew Quijano
-module InstructionMemory (
-    input [15:0] address,
-    output [15:0] instruction
-);
-    reg [15:0] memory [0:1023];
-
-    initial begin
-        memory[0] = 16'h2009;
-        memory[1] = 16'h200A;
-        memory[2] = 16'h012A;
-        memory[3] = 16'h012B;
-        memory[4] = 16'h014B;
-        memory[5] = 16'h014B;
-        memory[6] = 16'h016A;
-        memory[7] = 16'h018B;
-        memory[8] = 16'hFFFF;
-    end
-
-    assign instruction = memory[address[9:0]];
 endmodule
 
 // Author(s): Joey Conrory, Abbie Mathew
 module CPU (
     input clock,
-    input reset
+    output [15:0] PC,
+    output [15:0] ALUOut,
+    output [15:0] IR
 );
-    wire [15:0] PC, nextPC, instruction, RD1, RD2, ALUOut, ALUIn2, immediate;
+    reg [15:0] PC_reg;
+    reg [15:0] IMemory[0:1023];
+    wire [15:0] NextPC, A, RD2, B, SignExtend;
     wire [3:0] ALUControl;
-    wire RegWrite, ALUSrc, Zero;
-    reg halt;
+    wire [1:0] WR;
+    wire RegDst, ALUSrc, RegWrite, Zero;
 
     initial begin
-        halt = 0;
+        IMemory[0] = 16'b0111_00_01_00001111;
+        IMemory[1] = 16'b0111_00_10_00000111;
+        IMemory[2] = 16'b0010_01_10_11_000000;
+        IMemory[3] = 16'b0001_01_11_10_000000;
+        IMemory[4] = 16'b0011_10_11_10_000000;
+        IMemory[5] = 16'b0000_10_11_11_000000;
+        IMemory[6] = 16'b0100_10_11_01_000000;
+        IMemory[7] = 16'b0110_11_10_01_000000;
+        IMemory[8] = 16'b0110_10_11_01_000000;
+        IMemory[9] = 16'hFFFF;
     end
 
-    PC program_counter (
-        .clock(clock),
-        .reset(reset),
-        .nextPC(halt ? PC : nextPC),
-        .PC(PC)
-    );
+    initial PC_reg = 0;
+    assign PC = PC_reg;
+    assign IR = IMemory[PC_reg >> 1];
 
-    InstructionMemory instr_mem (
-        .address(PC),
-        .instruction(instruction)
-    );
-
-    ControlUnit control_unit (
-        .opcode(instruction[15:12]),
+    ControlUnit MainCtr(
+        .Op(IR[15:12]),
+        .RegDst(RegDst),
+        .ALUSrc(ALUSrc),
         .RegWrite(RegWrite),
-        .ALUControl(ALUControl),
-        .ALUSrc(ALUSrc)
+        .ALUControl(ALUControl)
     );
 
-    RegisterFile register_file (
-        .RR1(instruction[11:10]),
-        .RR2(instruction[9:8]),
-        .WR(instruction[7:6]),
+    RegisterFile rf(
+        .RR1(IR[11:10]),
+        .RR2(IR[9:8]),
+        .WR(WR),
         .WD(ALUOut),
         .RegWrite(RegWrite),
         .clock(clock),
-        .RD1(RD1),
+        .RD1(A),
         .RD2(RD2)
     );
 
-    assign immediate = {{8{instruction[7]}}, instruction[7:0]};
+    assign SignExtend = {{8{IR[7]}}, IR[7:0]};
 
-    assign ALUIn2 = (ALUSrc) ? immediate : RD2;
+    assign WR = (RegDst) ? IR[7:6] : IR[9:8];
 
-    ALU alu (
-        .A(RD1),
-        .B(ALUIn2),
+    assign B = (ALUSrc) ? SignExtend : RD2;
+
+    ALU ex(
         .ALUControl(ALUControl),
+        .A(A),
+        .B(B),
         .ALUOut(ALUOut),
         .Zero(Zero)
     );
 
-    always @(posedge clock or posedge reset) begin
-        if (reset)
-            halt <= 0;
-        else if (instruction == 16'hFFFF)
-            halt <= 1;
-    end
+    ALU fetch(
+        .ALUControl(4'b0010),
+        .A(PC_reg),
+        .B(16'd2),
+        .ALUOut(NextPC),
+        .Zero()
+    );
 
-    assign nextPC = PC + 16'h0002;
-
+    always @(negedge clock)
+        PC_reg <= NextPC;
 endmodule
 
 // Author(s): Joey Conroy, Abbie Mathew
 module CPU_tb;
     reg clock;
-    reg reset;
+    wire [15:0] ALUOut, IR, PC;
 
-    CPU uut (
+    CPU test_cpu(
         .clock(clock),
-        .reset(reset)
+        .PC(PC),
+        .ALUOut(ALUOut),
+        .IR(IR)
     );
 
-    initial begin
-        clock = 0;
-        reset = 1;
-        #10 reset = 0;
-        #100 $finish;
-    end
-
-    always #5 clock = ~clock;
+    always #1 clock = ~clock;
 
     initial begin
-        $display("Clock PC   IR                  WD");
-        $monitor("%b     %2d   %b  %3d (%b)", clock, uut.PC, uut.instruction, uut.ALUOut, uut.ALUOut);
+        $display("Clock PC   IR                 WD");
+        $monitor("%b     %2d   %b  %3d (%b)", clock, PC, IR, ALUOut, ALUOut);
+        clock = 1;
+        #34 $finish;
     end
 endmodule
