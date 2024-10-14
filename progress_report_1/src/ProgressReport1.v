2a43d3e919c1f88f56dd9f464e5b82d24d0c5c9b
Matthew Quijano <mq022002@gmail.com>
2024-10-14 13:12:48 -0400

    refactor: Better definition of logical flow separation
    #7

--- progress_report_1/src/ProgressReport1.v
+++ progress_report_1/src/ProgressReport1.v
@@ -1,32 +1,32 @@
+// === START OF UTILITY MODULES ===
 // Author(s): Joey Conroy, Abbie Mathew, Matthew Quijano
-// Module: ALU
-module ALU (op, a, b, result, zero);
-    input [3:0] op;
-    input [15:0] a, b;
-    output [15:0] result;
-    output zero;
-    wire [15:0] carry;
-    wire set;
-    ALU1 alu0 (a[0], b[0], op[3], op[2], op[1:0], set, op[2], carry[0], result[0]);
-    ALU1 alu1 (a[1], b[1], op[3], op[2], op[1:0], 1'b0, carry[0], carry[1], result[1]);
-    ALU1 alu2 (a[2], b[2], op[3], op[2], op[1:0], 1'b0, carry[1], carry[2], result[2]);
-    ALU1 alu3 (a[3], b[3], op[3], op[2], op[1:0], 1'b0, carry[2], carry[3], result[3]);
-    ALU1 alu4 (a[4], b[4], op[3], op[2], op[1:0], 1'b0, carry[3], carry[4], result[4]);
-    ALU1 alu5 (a[5], b[5], op[3], op[2], op[1:0], 1'b0, carry[4], carry[5], result[5]);
-    ALU1 alu6 (a[6], b[6], op[3], op[2], op[1:0], 1'b0, carry[5], carry[6], result[6]);
-    ALU1 alu7 (a[7], b[7], op[3], op[2], op[1:0], 1'b0, carry[6], carry[7], result[7]);
-    ALU1 alu8 (a[8], b[8], op[3], op[2], op[1:0], 1'b0, carry[7], carry[8], result[8]);
-    ALU1 alu9 (a[9], b[9], op[3], op[2], op[1:0], 1'b0, carry[8], carry[9], result[9]);
-    ALU1 alu10 (a[10], b[10], op[3], op[2], op[1:0], 1'b0, carry[9], carry[10], result[10]);
-    ALU1 alu11 (a[11], b[11], op[3], op[2], op[1:0], 1'b0, carry[10], carry[11], result[11]);
-    ALU1 alu12 (a[12], b[12], op[3], op[2], op[1:0], 1'b0, carry[11], carry[12], result[12]);
-    ALU1 alu13 (a[13], b[13], op[3], op[2], op[1:0], 1'b0, carry[12], carry[13], result[13]);
-    ALU1 alu14 (a[14], b[14], op[3], op[2], op[1:0], 1'b0, carry[13], carry[14], result[14]);
-    ALUmsb alu15 (a[15], b[15], op[3], op[2], op[1:0], 1'b0, carry[14], carry[15], result[15], set);
-    nor nor1(zero, result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7], 
-             result[8], result[9], result[10], result[11], result[12], result[13], result[14], result[15]);
+module Mux2To1 #(parameter N = 1) (input [N-1:0] a, input [N-1:0] b, input sel, output [N-1:0] y);
+    genvar i;
+    generate
+        for (i = 0; i < N; i = i + 1) begin: mux_loop
+            wire sel_n, a_and_sel_n, b_and_sel;
+            not (sel_n, sel);
+            and (a_and_sel_n, a[i], sel_n);
+            and (b_and_sel, b[i], sel);
+            or  (y[i], a_and_sel_n, b_and_sel);
+        end
+    endgenerate
 endmodule
 
+// Author(s): Joey Conroy, Abbie Mathew, Matthew Quijano
+module Mux4To1(input in0, input in1, input in2, input in3, input [1:0] sel, output y);
+    wire sel0_n, sel1_n, and0, and1, and2, and3;
+    not (sel0_n, sel[0]);
+    not (sel1_n, sel[1]);
+    and (and0, in0, sel1_n, sel0_n);
+    and (and1, in1, sel1_n, sel[0]);
+    and (and2, in2, sel[1], sel0_n);
+    and (and3, in3, sel[1], sel[0]);
+    or (y, and0, and1, and2, and3);
+endmodule
+// === END OF UTILITY MODULES ===
+
+// === START OF ALU-RELATED MODULES ===
 // Author(s): Joey Conroy, Abbie Mathew, Matthew Quijano
 module ALU1 (a, b, ainvert, binvert, op, less, carryin, carryout, result);
     input a, b, less, carryin, ainvert, binvert;
@@ -66,33 +66,36 @@ module ALUmsb (a, b, ainvert, binvert, op, less, carryin, carryout, result, sum)
 endmodule
 
 // Author(s): Joey Conroy, Abbie Mathew, Matthew Quijano
-module Mux2To1 #(parameter N = 1) (input [N-1:0] a, input [N-1:0] b, input sel, output [N-1:0] y);
-    genvar i;
-    generate
-        for (i = 0; i < N; i = i + 1) begin: mux_loop
-            wire sel_n, a_and_sel_n, b_and_sel;
-            not (sel_n, sel);
-            and (a_and_sel_n, a[i], sel_n);
-            and (b_and_sel, b[i], sel);
-            or  (y[i], a_and_sel_n, b_and_sel);
-        end
-    endgenerate
+module ALU (op, a, b, result, zero);
+    input [3:0] op;
+    input [15:0] a, b;
+    output [15:0] result;
+    output zero;
+    wire [15:0] carry;
+    wire set;
+    ALU1 alu0 (a[0], b[0], op[3], op[2], op[1:0], set, op[2], carry[0], result[0]);
+    ALU1 alu1 (a[1], b[1], op[3], op[2], op[1:0], 1'b0, carry[0], carry[1], result[1]);
+    ALU1 alu2 (a[2], b[2], op[3], op[2], op[1:0], 1'b0, carry[1], carry[2], result[2]);
+    ALU1 alu3 (a[3], b[3], op[3], op[2], op[1:0], 1'b0, carry[2], carry[3], result[3]);
+    ALU1 alu4 (a[4], b[4], op[3], op[2], op[1:0], 1'b0, carry[3], carry[4], result[4]);
+    ALU1 alu5 (a[5], b[5], op[3], op[2], op[1:0], 1'b0, carry[4], carry[5], result[5]);
+    ALU1 alu6 (a[6], b[6], op[3], op[2], op[1:0], 1'b0, carry[5], carry[6], result[6]);
+    ALU1 alu7 (a[7], b[7], op[3], op[2], op[1:0], 1'b0, carry[6], carry[7], result[7]);
+    ALU1 alu8 (a[8], b[8], op[3], op[2], op[1:0], 1'b0, carry[7], carry[8], result[8]);
+    ALU1 alu9 (a[9], b[9], op[3], op[2], op[1:0], 1'b0, carry[8], carry[9], result[9]);
+    ALU1 alu10 (a[10], b[10], op[3], op[2], op[1:0], 1'b0, carry[9], carry[10], result[10]);
+    ALU1 alu11 (a[11], b[11], op[3], op[2], op[1:0], 1'b0, carry[10], carry[11], result[11]);
+    ALU1 alu12 (a[12], b[12], op[3], op[2], op[1:0], 1'b0, carry[11], carry[12], result[12]);
+    ALU1 alu13 (a[13], b[13], op[3], op[2], op[1:0], 1'b0, carry[12], carry[13], result[13]);
+    ALU1 alu14 (a[14], b[14], op[3], op[2], op[1:0], 1'b0, carry[13], carry[14], result[14]);
+    ALUmsb alu15 (a[15], b[15], op[3], op[2], op[1:0], 1'b0, carry[14], carry[15], result[15], set);
+    nor nor1(zero, result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7], 
+             result[8], result[9], result[10], result[11], result[12], result[13], result[14], result[15]);
 endmodule
+// === END OF ALU-RELATED MODULES ===
 
-// Author(s): Joey Conroy, Abbie Mathew, Matthew Quijano
-module Mux4To1(input in0, input in1, input in2, input in3, input [1:0] sel, output y);
-    wire sel0_n, sel1_n, and0, and1, and2, and3;
-    not (sel0_n, sel[0]);
-    not (sel1_n, sel[1]);
-    and (and0, in0, sel1_n, sel0_n);
-    and (and1, in1, sel1_n, sel[0]);
-    and (and2, in2, sel[1], sel0_n);
-    and (and3, in3, sel[1], sel[0]);
-    or (y, and0, and1, and2, and3);
-endmodule
-
+// === START OF CORE COMPONENTS ===
 // Author(s): Joey Conroy
-// Module: RegisterFile
 module RegisterFile (
     input [1:0] RR1, RR2,             
     input [1:0] WR,                   
@@ -116,7 +119,6 @@ module RegisterFile (
 endmodule
 
 // Author(s): Abbie Mathew
-// Module: InstructionMemory
 module InstructionMemory (
     input [15:0] Address,
     output [15:0] Instruction
@@ -138,7 +140,6 @@ module InstructionMemory (
 endmodule
 
 // Author(s): Matthew Quijano
-// Module: ControlUnit
 module ControlUnit (
     input [3:0] Op,
     output reg RegDst,
@@ -204,9 +205,10 @@ module ControlUnit (
             end
         endcase
 endmodule
+// === END OF CORE COMPONENTS ===
 
+// === START OF CPU ===
 // Author(s): Joey Conroy, Abbie Mathew, Matthew Quijano
-// Module: CPU
 module CPU (
     input clock,
     output [15:0] PC,
@@ -239,9 +241,10 @@ module CPU (
             PC_reg <= NextPC;
     end
 endmodule
+// === END OF CPU ===
 
+// === START OF TESTBENCH ===
 // Author(s): Joey Conroy, Abbie Mathew
-// Module: CPU_tb
 module CPU_tb;
     reg clock;
     wire signed [15:0] ALUOut, IR, PC;
@@ -259,6 +262,7 @@ module CPU_tb;
         $finish;
     end
 endmodule
+// === END OF TESTBENCH ===
 
 /*
 Clock PC   IR                    WD
